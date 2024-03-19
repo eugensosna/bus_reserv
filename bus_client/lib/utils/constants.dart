ebbf0ecad78714a51c4b717ddb2c870c322f3d67
Evhen Sosna <eugensosna@gmx.de>
2024-03-19 21:40:18 +0100

    fix consstants

--- bus_client/lib/utils/constants.dart
+++ bus_client/lib/utils/constants.dart
@@ -7,3 +7,16 @@ const busTypes = [busTypeACBusiness, busTypeACEconomy, busTypeNonAc];
 const String busTypeNonAc = 'NON-AC';
 const String busTypeACEconomy = 'AC-ECONOMY';
 const String busTypeACBusiness = 'AC-BUSINESS';
+
+
+const cities = [
+  'Dhaka',
+  'Chittagong',
+  'Sylhet',
+  'Bandarban',
+  'Rajshahi',
+  'Khagrachari',
+  'Rangamati',
+];
+
+const seatLabelList  = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
