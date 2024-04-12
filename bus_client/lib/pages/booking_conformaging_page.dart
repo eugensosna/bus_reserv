75ee30505f89f390e7d68715863ec88da4dee198
Evhen Sosna <eugensosna@gmx.de>
2024-04-12 21:59:05 +0200

    boking page

---
+++ bus_client/lib/pages/booking_conformaging_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BokkingConformationPage  extends StatefulWidget {
+  const BokkingConformationPage ({Key? key}): super(key: key);
+
+  @override
+  State<BokkingConformationPage > createState() =>
+      _BokkingConformationPageState();
+}
+
+class _BokkingConformationPageState  extends State<BokkingConformationPage > {
+  late String departureDate;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
