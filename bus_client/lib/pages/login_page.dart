6e40c9d24a91d1882df54e256d405699c3d78ff4
Evhen Sosna <eugensosna@gmx.de>
2024-04-23 22:20:59 +0200

    fix login page bugs

--- bus_client/lib/pages/login_page.dart
+++ bus_client/lib/pages/login_page.dart
@@ -4,17 +4,12 @@ class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Container(),
-    );
+    return const Placeholder();
   }
 }
